04da5af8106fe45b85df2677fabaf32562e095c8
Sergei Shchipanov <sshchipanov@microsoft.com>
2025-05-27 12:52:17 +0300

    initial commit

---
+++ src/main/java/com/sshchipanov/parser/RostersParserApplication.java
@@ -0,0 +1,13 @@
+package com.sshchipanov.parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RostersParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RostersParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sshchipanov/parser/RostersParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sshchipanov.parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RostersParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
